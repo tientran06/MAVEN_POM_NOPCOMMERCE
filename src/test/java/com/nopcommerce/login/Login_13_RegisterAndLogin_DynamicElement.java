715f8b66175e2a5b186bead8f24c6caf7b54a152
Tien Tran <titran06@gmail.com>
2020-07-05 16:11:47 +0700

    update TC_05

--- src/test/java/com/nopcommerce/login/Login_13_RegisterAndLogin_DynamicElement.java
+++ src/test/java/com/nopcommerce/login/Login_13_RegisterAndLogin_DynamicElement.java
@@ -2,7 +2,6 @@ package com.nopcommerce.login;
 
 import org.testng.annotations.Test;
 
-
 import commons.AbstractTest;
 import pageObjects.nopcommerce.HeaderDeskTopPageObject;
 import pageObjects.nopcommerce.HeaderApparelPageObject;
@@ -16,7 +15,6 @@ import pageObjects.nopcommerce.RegisterPageObject;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
-
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
@@ -34,7 +32,7 @@ public class Login_13_RegisterAndLogin_DynamicElement extends AbstractTest {
 	private HeaderComputersPageObject computersPage;
 	private HeaderDeskTopPageObject desktopPage;
 
-	@Parameters({"browser","url"})
+	@Parameters({ "browser", "url" })
 	@BeforeTest
 	public void beforeTest(String browserName, String url) {
 		driver = getBrowserDriver(browserName, url);
@@ -50,17 +48,18 @@ public class Login_13_RegisterAndLogin_DynamicElement extends AbstractTest {
 		password = "123456";
 		confirmPassword = "123456";
 	}
+
 	@Test
 	public void TC_01_RegisterToSystem() {
 		// Click to Register >> Register page
 		homePage.clickToHeaderLinkPagesByName(driver, "Register");
 		registerPage = PageGeneratorManager.getRegisterPage(driver);
-		
+
 		// Register page >> input First name / Last name
 		registerPage.clickToNopCommerceRadioButtonByID(driver, "gender-female");
 		registerPage.inputToNopCommerceTextboxByID(driver, "FirstName", firstName);
 		registerPage.inputToNopCommerceTextboxByID(driver, "LastName", lastName);
-		
+
 		// Select Date of Birth
 		registerPage.selectNopCommerceDropdownByName(driver, "DateOfBirthDay", "2");
 		registerPage.selectNopCommerceDropdownByName(driver, "DateOfBirthMonth", "June");
@@ -71,10 +70,10 @@ public class Login_13_RegisterAndLogin_DynamicElement extends AbstractTest {
 		registerPage.inputToNopCommerceTextboxByID(driver, "Company", companyName);
 		registerPage.inputToNopCommerceTextboxByID(driver, "Password", password);
 		registerPage.inputToNopCommerceTextboxByID(driver, "ConfirmPassword", confirmPassword);
-		
+
 		registerPage.clickToNopCommerceButtonByValue(driver, "Register");
 		verifyTrue(registerPage.isRegisterSuccessMsgDisplayed("Your registration completed"));
-		
+
 		// Click Log out link >> HomePage
 		registerPage.clickToHeaderLinkPagesByName(driver, "Log out");
 		homePage = PageGeneratorManager.getHomePage(driver);
@@ -92,8 +91,8 @@ public class Login_13_RegisterAndLogin_DynamicElement extends AbstractTest {
 		// Click to Login button >> Home Page
 		loginPage.clickToNopCommerceButtonByValue(driver, "Log in");
 		homePage = PageGeneratorManager.getHomePage(driver);
-		
-		//Verify log in successfully
+
+		// Verify log in successfully
 		verifyTrue(homePage.isMyAccountLinkDisplayed());
 
 	}
@@ -103,41 +102,46 @@ public class Login_13_RegisterAndLogin_DynamicElement extends AbstractTest {
 		// 1.Home Page > Computers
 		homePage.openHeaderMenuPagesByName(driver, "Computers");
 		computersPage = PageGeneratorManager.getHeaderComputerPage(driver);
-		computersPage.sleepInSecond(driver,2);
+		computersPage.sleepInSecond(driver, 2);
 
 		// 2. Computers > Apparel
 		computersPage.openHeaderMenuPagesByName(driver, "Apparel");
 		apparelPage = PageGeneratorManager.getHeaderApparelPage(driver);
-		apparelPage.sleepInSecond(driver,2);
+		apparelPage.sleepInSecond(driver, 2);
 
 		// 3. Apparel > GiftCards
 		apparelPage.openHeaderMenuPagesByName(driver, "Gift Cards");
 		giftCardsPage = PageGeneratorManager.getHeaderGiftCardsPage(driver);
-		giftCardsPage.sleepInSecond(driver,2);
+		giftCardsPage.sleepInSecond(driver, 2);
 
 		// 4. GiftCards > BooksPage
 		giftCardsPage.openHeaderMenuPagesByName(driver, "Books");
 		booksPage = PageGeneratorManager.getHeaderBooksPage(driver);
-		booksPage.sleepInSecond(driver,2);
+		booksPage.sleepInSecond(driver, 2);
 
 		// 5. BooksPage > Homepage
 		booksPage.openHomePage(driver);
 		homePage = PageGeneratorManager.getHomePage(driver);
-		homePage.sleepInSecond(driver,2);
+		homePage.sleepInSecond(driver, 2);
 	}
+
 	@Test(description = "only use in case more pages")
-		public void TC_04_Dymamic_Header_Sub_Menu_More() {
-			// 1.Home Page > Computers >> Desktops
-			homePage.openHeaderSubMenuPagesByName(driver, "Computers", "Desktops");
-			desktopPage = PageGeneratorManager.getHeaderDeskTopPage(driver);
-			desktopPage.sleepInSecond(driver,2);
+	public void TC_04_Dymamic_Header_Sub_Menu_More() {
+		// 1.Home Page > Computers >> Desktops
+		homePage.openHeaderSubMenuPagesByName(driver, "Computers", "Desktops");
+		desktopPage = PageGeneratorManager.getHeaderDeskTopPage(driver);
+		desktopPage.sleepInSecond(driver, 2);
 
-			// 2. Computers > Apparel
-			desktopPage.openHeaderSubMenuPagesByName(driver, "Apparel", "Clothing");
-			apparelPage = PageGeneratorManager.getHeaderApparelPage(driver);
-			apparelPage.sleepInSecond(driver,2);
-		}
+		// 2. Computers > Apparel
+		desktopPage.openHeaderSubMenuPagesByName(driver, "Apparel", "Clothing");
+		apparelPage = PageGeneratorManager.getHeaderApparelPage(driver);
+		apparelPage.sleepInSecond(driver, 2);
+	}
 
+	@Test(description = "only use in case more pages")
+	public void TC_05_Dymamic_Header_Menu_More() {
+		
+	}
 	@AfterTest(alwaysRun = true)
 	public void afterTest() {
 		closeBrowserAndDriver(driver);
